2b08b2732bf41e5fcf1a42ea00f1969fd454dac5
Ashwin Gundarapu <linuxuser509@outlook.com>
2026-06-26 13:10:57 +0530

    fix: network-first service worker to prevent stale cache on deploy

--- sw.js
+++ sw.js
@@ -1,15 +1,26 @@
+var CACHE = 'mindful-bharat-v2';
+
 self.addEventListener('install', function(e) {
+  self.skipWaiting();
   e.waitUntil(
-    caches.open('mindful-bharat-v1').then(function(c) {
-      return c.addAll(['./', './index.html', './manifest.json']);
+    caches.open(CACHE).then(function(c) {
+      return c.addAll(['./manifest.json']);
+    })
+  );
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(keys.filter(function(k) { return k !== CACHE; }).map(function(k) { return caches.delete(k); }));
     })
   );
 });
 
 self.addEventListener('fetch', function(e) {
   e.respondWith(
-    caches.match(e.request).then(function(r) {
-      return r || fetch(e.request);
+    fetch(e.request).catch(function() {
+      return caches.match(e.request);
     })
   );
 });
